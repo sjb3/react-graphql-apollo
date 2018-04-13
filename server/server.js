842d026ceda4801f99949e00eb6fc5b259fb1599
Sung J Byun <sungjbyun@D-108-179-180-61.dhcp4.washington.edu>
2018-04-12 17:49:23 -0700

    finished with materialize; need to review more of Apollo

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import cors from 'cors';
+import { execute, subscribe } from 'graphql';
+import { createServer } from 'http';
+import { SubscriptionServer } from 'subscriptions-transport-ws';
 import { schema } from './src/schema';
 
 const myGraphQLSchema = schema;
@@ -11,8 +14,22 @@ const server = express();
 server.use('*', cors({ origin: 'http://localhost:3000'}));
 
 server.use('/graphql', bodyParser.json(), graphqlExpress({ schema: myGraphQLSchema}));
-server.use('/graphiql', bodyParser.json(), graphiqlExpress({ endpointURL: '/graphql'}));
+server.use('/graphiql', bodyParser.json(), graphiqlExpress({ 
+  endpointURL: '/graphql',
+  subscriptionsEndpoint: `ws://localhost:${PORT}/subscriptions`
+}));
 
-server.listen(PORT, () =>
-  console.log(`Your GraphQL server is running on port ${PORT}`)
-);
+const ws = createServer(server);
+
+ws.listen(PORT, () => {
+  console.log(`Your GraphQL server is running on port ${PORT}`);
+
+  new SubscriptionServer({
+    execute,
+    subscribe,
+    schema
+  }, {
+    server: ws,
+    path: '/subscriptions'
+  })
+});
--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,33 +1,84 @@
+import { PubSub, withFilter } from 'graphql-subscriptions';
+
+const pubsub = new PubSub();
+
 const contacts = [
   {
-    id: 1,
+    id: '1',
     firstName: 'Manny',
-    lastName: 'Henri'
+    lastName: 'Henri',
+    notes: [
+      { 
+        id: '1',
+        details: 'I think this guy is an author at linkedin'
+      },
+      { 
+        id: '2',
+        details: 'His name is Manny'
+      }
+    ]
   },
   {
-    id: 2,
+    id: '2',
     firstName: 'Jasmine',
-    lastName: 'Henri-Rainville'
+    lastName: 'Henri-Rainville',
+    notes: [
+      { 
+        id: '1',
+        details: 'I think this guy is an author at linkedin'
+      },
+      { 
+        id: '2',
+        details: 'His name is Manny'
+      }
+    ]
   },
     {
-    id: 3,
+    id: '3',
     firstName: 'Jeremy',
-    lastName: 'Henri-Rainville'
+    lastName: 'Henri-Rainville',
+    notes: [
+      { 
+        id: '1',
+        details: 'I think this guy is an author at linkedin'
+      },
+      { 
+        id: '2',
+        details: 'His name is Manny'
+      }
+    ]
   }
 ]
 
 export const resolvers = {
   Query: {
     contacts: () => {
-      return contacts;   
+      return contacts;
+    },
+    contact: (root, { id }) => {
+      return contacts.find(contact => contact.id === id);
     },
   },
   Mutation: {
     addContact: (root, args) => {
-      const newId = require('crypto').randomBytes(5).toString('hex');
-      const newContact = { id: newId, firstName: args.firstName, lastName: args.lastName};
+      const newContact = { id: args.id, firstName: args.firstName, lastName: args.lastName, notes: [] };
       contacts.push(newContact);
       return newContact;
+    },
+    addNote: (root, { note }) => {
+      const newId = require('crypto').randomBytes(5).toString('hex');
+      const contact = contacts.find(contact => contact.id === note.contactId);
+      const newNote = { id: String(newId), details: note.details };
+      contact.notes.push(newNote);
+      pubsub.publish('noteAdded', { noteAdded: newNote, contactId: note.contactId });
+      return newNote;
     }
-  }
+  },
+  Subscription: {
+    noteAdded: {
+      subscribe: withFilter(() => pubsub.asyncIterator('noteAdded'), (payload, variables) => {
+        return payload.contactId === variables.contactId;
+      }),
+    }
+  },
 };
